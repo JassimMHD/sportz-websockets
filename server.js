a322597a0c259442b6c12322dd883dcc2b3898e3
JassimMHD <jassimjr10you@gmail.com>
2026-02-02 22:59:52 +0530

    implement arcjet secuirity

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 import express from "express";
+import arcjet from "@arcjet/node";
 import http from "http";
 import { matchesRouter } from "./src/routes/matches.js";
 import { attachWebSocketServer } from "./src/ws/server.js";
+import { secuirtyMiddleware } from "./src/arcjet.js";
 
 const app = express();
 const PORT = Number(process.env.PORT) || 8000;
@@ -14,13 +16,16 @@ app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
+app.use(secuirtyMiddleware());
+
 app.use("/matches", matchesRouter);
 
 const { broadcastMatchCreated } = attachWebSocketServer(server);
 app.locals.broadcastMatchCreated = broadcastMatchCreated;
 
 server.listen(PORT, HOST, () => {
-    const baseUrl = HOST === '0.0.0.0' ? `http://localhost:${PORT}` : `http://${HOST}:${PORT}`; 
+  const baseUrl =
+    HOST === "0.0.0.0" ? `http://localhost:${PORT}` : `http://${HOST}:${PORT}`;
   console.log(`Server is running on ${baseUrl}`);
-  console.log(`WebSocket server is running on ws://localhost:${PORT}/ws`);    
+  console.log(`WebSocket server is running on ws://localhost:${PORT}/ws`);
 });
